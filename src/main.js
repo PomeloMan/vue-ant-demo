b0f4917673c904a752e609e36c09d36dd92abe86
fengchao <chao.feng9@pactera.com>
2019-11-22 15:42:44 +0800

    feat: 添加引入 Ant Design 说明

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ import router from './configs/router'
 import store from './configs/store'
 import http from './configs/http'
 
+// 引入 Ant Design，https://www.antdv.com/docs/vue/getting-started-cn/
 // 完整引入 ant design 库
 // import Antd from 'ant-design-vue'
 // import 'ant-design-vue/dist/antd.css'
 // Vue.use(Antd)
-// 按需引入 ant design 库
+
+// 按需引入 ant design 库，需要配置 babel.config.js
+// plugins: [
+//   [
+//     "import",
+//     { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
+//   ]
+// ]
 import { Icon } from 'ant-design-vue'
 Vue.component(Icon.name, Icon)
 
