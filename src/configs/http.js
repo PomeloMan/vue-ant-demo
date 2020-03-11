d6ed22483e548bd01d5a982b328c4c3f6652c7ba
fengchao <chao.feng9@pactera.com>
2020-03-11 13:47:42 +0800

    feat: 添加withCredentials配置说明

--- src/configs/http.js
+++ src/configs/http.js
@@ -5,6 +5,9 @@ const http = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
   timeout: 1000 * 30,
   withCredentials: true,
+  // (一) 当前端配置withCredentials=true时, 后端配置Access-Control-Allow-Origin不能为*, 必须是相应地址
+  // (二) 当配置withCredentials=true时, 后端需配置Access-Control-Allow-Credentials
+  // (三) 当前端配置请求头时, 后端需要配置Access-Control-Allow-Headers为对应的请求头集合
   headers: {
     'Content-Type': 'application/json; charset=utf-8'
   }
